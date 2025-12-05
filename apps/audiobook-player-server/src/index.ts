2fac8807b3ee56e95ddd8b1b532d75eb3e10eb53
Andrei Maksimovich <andrzej.m@amaxsoftware.com>
2025-12-05 14:15:15 +0400

    Added support for local HTTPS connections for both the server and client.

--- apps/audiobook-player-server/src/index.ts
+++ apps/audiobook-player-server/src/index.ts
@@ -1,9 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import express, {Express, json} from 'express';
 import cors from 'cors';
-
+import * as https from "https"
 import {apiRouter} from "./web-service/api/ApiRoute";
 import fs from "fs";
 
@@ -25,6 +24,23 @@ if (filesPath !== null) {
     app.use('/files', express.static(filesPath))
 }
 
-app.listen(WEB_SERVICE_PORT, () => {
-    console.log(`Web service started on port ${process.env.WEB_SERVICE_PORT}`);
-});
+// HTTPS
+if (process.env.SSL_ENABLED) {
+    const options = {
+        key: fs.readFileSync(process.env.SSL_KEY_PATH!).toString(),
+        cert: fs.readFileSync(process.env.SSL_CERT_PATH!).toString(),
+    }
+    const server = https.createServer(options, app);
+    server.listen(WEB_SERVICE_PORT, () => {
+        console.log(`Web service started on port ${process.env.WEB_SERVICE_PORT} in HTTPS mode`);
+    })
+}
+
+// HTTP
+else {
+    app.listen(WEB_SERVICE_PORT, () => {
+        console.log(`Web service started on port ${process.env.WEB_SERVICE_PORT} in HTTP mode`);
+    });
+}
+
+
